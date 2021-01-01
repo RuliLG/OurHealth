21fb198ef941e4fefa3135a437a4faa36c8f8e5c
Raúl López <hi@raullg.com>
2021-01-01 18:30:06 +0000

    Create 2020_12_29_154854_create_patients_table.php

---
+++ ourhealth/database/migrations/2020_12_29_154854_create_patients_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 60);
+            $table->string('middle_name', 60)->nullable();
+            $table->string('last_name', 60)->nullable();
+            $table->string('id_card', 120);
+            $table->string('nationality', 4)->nullable();
+            $table->string('original_region', 4)->nullable();
+            $table->string('photo_s3_key')->nullable();
+            $table->foreignId('preferred_hospital_id')->nullable()->constrained('hospitals')->nullOnDelete();
+            $table->date('date_of_birth')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('email')->nullable();
+            $table->enum('blood_type', ['A+', 'B+', 'AB-', 'AB+', 'A-', 'B-', '0-', '0+'])->nullable();
+            $table->foreignId('third_party_insurance_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('doctor_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('biological_father_id')->nullable()->constrained('patients')->nullOnDelete();
+            $table->foreignId('biological_mother_id')->nullable()->constrained('patients')->nullOnDelete();
+            $table->timestamps();
+
+            $table->unique(['id_card', 'nationality']);
+            $table->foreign('nationality')->references('iso_code')->on('countries')->nullOnDelete();
+            $table->foreign('original_region')->references('iso_code')->on('countries')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
