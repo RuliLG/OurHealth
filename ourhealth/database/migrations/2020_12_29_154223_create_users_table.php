fd574c364780e7753e24a4a8c1e01d9580e08163
Raúl López <hi@raullg.com>
2021-01-01 18:17:28 +0000

    Created users table

--- ourhealth/database/migrations/2020_12_29_154223_create_users_table.php
+++ ourhealth/database/migrations/2020_12_29_154223_create_users_table.php
@@ -16,9 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_active')->default(false);
+            $table->string('profile_picture_s3_key')->nullable();
+            $table->foreignId('hospital_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('department_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('position')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
