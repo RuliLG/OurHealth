312d874b375b3bbee90261f94b168127b844d8ea
Raúl López <hi@raullg.com>
2021-01-01 19:19:15 +0000

    Create Measurement.php

---
+++ ourhealth/app/Models/Measurement.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Measurement extends Model
+{
+    use HasFactory;
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function visit()
+    {
+        return $this->belongsTo(Visit::class);
+    }
+}
