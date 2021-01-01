850c75798c2db6b6c50fd36201af237a6d458a49
Raúl López <hi@raullg.com>
2021-01-01 18:32:22 +0000

    Create 2020_12_29_154957_create_measurements_table.php

---
+++ ourhealth/database/migrations/2020_12_29_154957_create_measurements_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeasurementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('measurements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('visit_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->enum('type', ['heart', 'blood', 'urine']); // TODO: Preguntar
+            $table->string('other_type', 40)->nullable();
+            $table->string('name');
+            $table->string('value');
+            $table->integer('batch_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('measurements');
+    }
+}
