364bda6cda51ae8d238b260b0971da4287dcb900
Raúl López <hi@raullg.com>
2021-01-01 18:14:11 +0000

    Create 2020_12_29_154155_create_regions_table.php

---
+++ ourhealth/database/migrations/2020_12_29_154155_create_regions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60)->index();
+            $table->string('country_iso_code', 4);
+            $table->timestamps();
+            $table->unique(['country_iso_code', 'name']);
+
+            $table->foreign('country_iso_code')->references('iso_code')->on('countries')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('regions');
+    }
+}
