e89f20138750895b8d3233a2bbc9fc07483dcba5
Raúl López <hi@raullg.com>
2021-01-01 19:16:55 +0000

    Create File.php

---
+++ ourhealth/app/Models/File.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+class File extends Model
+{
+    use HasFactory;
+
+    protected $appends = [
+        'file_path',
+    ];
+    protected $hidden = [
+        's3_key',
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function visit()
+    {
+        return $this->belongsTo(Visit::class);
+    }
+
+    public function getFilePathAttribute()
+    {
+        return Storage::url($this->s3_key);
+    }
+}
