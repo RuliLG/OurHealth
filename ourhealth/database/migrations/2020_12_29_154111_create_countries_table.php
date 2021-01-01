f3f810e9322a9f4cb8ba47773985449dc50fdd2c
Raúl López <hi@raullg.com>
2021-01-01 18:14:00 +0000

    Create 2020_12_29_154111_create_countries_table.php

---
+++ ourhealth/database/migrations/2020_12_29_154111_create_countries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->string('iso_code', 4)->unique();
+            $table->string('name', 50);
+            $table->string('flag_icon_s3_key')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
